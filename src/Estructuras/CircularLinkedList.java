7aea6a159b43994c417ec63d8cef47f7b47a8813
SrJowy <joelbraortiz@gmail.com>
2020-10-19 15:59:04 +0200

    commit2

--- src/Estructuras/CircularLinkedList.java
+++ src/Estructuras/CircularLinkedList.java
@@ -7,6 +7,7 @@ import Interfaces.ListADT;
 public class CircularLinkedList<T> implements ListADT<T>{
 
         protected Node<T> last;
+        
         protected String descr;
         protected int count;
 
