43a06683c35fcf37a18472f410cb9877b352cf3c
SrJowy <joelbraortiz@gmail.com>
2020-10-23 16:55:34 +0200

    Hechos merge y add de OrderedCircularLinkedList

--- src/Estructuras/OrderedCircularLinkedList.java
+++ src/Estructuras/OrderedCircularLinkedList.java
@@ -1,6 +1,6 @@
 package Estructuras;
 import Interfaces.OrderedListADT;
-public class OrderedCircularLinkedList<T> extends CircularLinkedList<T> implements OrderedListADT<T>{
+public class OrderedCircularLinkedList<T extends Comparable<T>> extends CircularLinkedList<T> implements OrderedListADT<T>{
     
     public OrderedCircularLinkedList(String descr) {
 		super(descr);
@@ -8,14 +8,37 @@ public class OrderedCircularLinkedList<T> extends CircularLinkedList<T> implemen
 
     @Override
     public void add(T elem) {
-        // TODO Auto-generated method stub
+        Node <T> nuevo = new Node<T>(elem);
+        Node <T> act = last.next;
+        Node <T> ant = last;
+        boolean inic = true;
 
+        while (act != last.next || inic) {
+            inic = false;
+            if (act.data.compareTo(nuevo.data) > 0) {
+                if (ant.next == last.next) {
+                    last = nuevo;
+                }
+                nuevo.next = act;
+                ant.next = nuevo;
+                count++;
+            }
+            ant = act;
+            act = act.next;
+        } 
     }
 
     @Override
     public void merge(OrderedCircularLinkedList<T> lista) {
-        // TODO Auto-generated method stub
-
+        Node <T> act = lista.last.next;
+        boolean inic = true;
+        if(lista.last != null) {
+            while (act == lista.last.next || inic) {
+                inic = false;
+                this.add(act.data);
+                act = act.next;
+            }
+        }
     }
 
     
--- src/Interfaces/OrderedListADT.java
+++ src/Interfaces/OrderedListADT.java
@@ -2,7 +2,7 @@
 package Interfaces;
 import Estructuras.OrderedCircularLinkedList;
 
-public interface OrderedListADT<T> extends ListADT<T> {
+public interface OrderedListADT<T extends Comparable<T>> extends ListADT<T> {
    public void add(T elem);
    public void merge(OrderedCircularLinkedList<T> lista);
 }
