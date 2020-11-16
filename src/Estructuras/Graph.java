4db63f6f93b65aba30dd2263607bd4d4bf9c2358
SrJowy <joelbraortiz@gmail.com>
2020-11-16 23:24:12 +0100

    Grafo con el método añadido

--- src/Estructuras/Graph.java
+++ src/Estructuras/Graph.java
@@ -3,6 +3,8 @@ package Estructuras;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
 
 import labo1.CatalogoWeb;
 import labo1.ListaWebs;
@@ -20,6 +22,7 @@ public class Graph {
     public void crearGrafo(ArrayList<Web> lista) {
         anadirAlHashMap(lista);
         llenarKeys(lista);
+        llenarAdjList();
     }
 
     private void anadirAlHashMap(ArrayList<Web> l) {
@@ -27,28 +30,77 @@ public class Graph {
         Iterator<Web> itr = l.iterator();
         int i = 0;
 
-        while (itr.hasNext()) {
+        while (itr.hasNext()) { 
             w = itr.next();
             this.tH.put(w.getLink(), i);
             i++;
         }
-        this.keys = new String[tH.size()];
+        //n = número de elementos en el ArrayList l
+        //Coste O(n)
     }
 
     private void llenarKeys(ArrayList<Web> l) {
-        for (Web w: l) {
-            int index = tH.get(w.getLink());
-            keys[index] = w.getLink();
-        }
+        keys = new String[tH.size()];
+        for (String k: tH.keySet()) keys[tH.get(k)] = k;
+        //n = número de elementos en el HashMap
+        //Coste O(n)
     }
 
     private void llenarAdjList() {
+        adjList = new ArrayList[keys.length];
         for (int i=0; i<keys.length; i++) {
-            ListaWebs lW = CatalogoWeb.getCatalogoWeb().buscarWeb(keys[i]).getListaEnlazadas();
+            ListaWebs lW = CatalogoWeb.getCatalogoWeb().buscarWeb(keys[i]).getListaEnlazadas(); //Coste: O(1)
             ArrayList<Integer> l = new ArrayList<>();
-            for (int j = 0; i<lW.getTamaino(); j++) {
-                l.add(tH.get(lW.getWebIndice(j).getLink()));
+            for (int j = 0; j<lW.getTamaino(); j++) {
+                l.add(tH.get(lW.getWebIndice(j).getLink())); //Coste O(1)
+            }
+            adjList[i] = l;
+        }
+        //n = número de webs de ListaWebs
+        //m = número de elementos en la Lista de enlazadas de una web
+        //Coste: O(n*m/2)
+    }
+
+    public void print(){
+        for (int i = 0; i < adjList.length; i++){
+         System.out.print("Element: " + i + " " + keys[i] + " --> ");
+         for (int k: adjList[i])  System.out.print(keys[k] + " ### ");
+         
+         System.out.println();
+        }
+     }
+
+     public boolean estanConectados(String a1, String a2){
+		Queue<Integer> porExaminar = new LinkedList<Integer>();
+		
+		int pos1 = tH.get(a1);
+		int pos2 = tH.get(a2);
+		boolean enc = false;
+		boolean[] examinados = new boolean[tH.size()];
+
+        for(int i = 0; i<adjList[pos1].size(); i++) {
+            int elem = adjList[pos1].get(i);
+            porExaminar.add(elem);
+            examinados[elem] = true;
+        }
+        if (!porExaminar.isEmpty()) pos1 = porExaminar.element();
+
+        while (!enc && !porExaminar.isEmpty()) {
+            for(int i = 0; i<adjList[pos1].size(); i++) {
+                int elem = adjList[pos1].get(i);
+                if (examinados[elem] != true) {
+                    porExaminar.add(elem);
+                    examinados[elem] = true;
+                } 
+            }
+            if (porExaminar.element() == pos2) enc = true;
+            else {
+                porExaminar.remove();
+                if (!porExaminar.isEmpty())
+                pos1 = porExaminar.element();
             }
         }
+		
+        return enc;
     }
 }
